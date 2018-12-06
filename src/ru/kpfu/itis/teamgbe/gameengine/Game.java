746bae005b95e3e139b37bb81285bc1a215e86a4
Alexandr Ermakov <supertux038@gmail.com>
2018-12-06 22:36:14 +0300

    Added game creator

--- src/ru/kpfu/itis/teamgbe/gameengine/Game.java
+++ src/ru/kpfu/itis/teamgbe/gameengine/Game.java
@@ -1,5 +1,15 @@
 package ru.kpfu.itis.teamgbe.gameengine;
 
 public class Game {
-    Cell[][] gameField; //git pushable
+    Cell[][] gameField;
+    private final int height;
+    private final int width;
+
+    public Game(int height, int width) {
+        this.height = height;
+        this.width = width;
+        gameField = new Cell[height][width];
+    }
+
+
 }
