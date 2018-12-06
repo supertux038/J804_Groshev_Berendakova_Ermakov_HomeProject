b9271285d9c7e62097509a92cd857b447ddfe93e
Alexandr Ermakov <supertux038@gmail.com>
2018-12-06 21:17:37 +0300

    Added cell class version 0.001

--- src/ru/kpfu/itis/teamgbe/gameengine/Cell.java
+++ src/ru/kpfu/itis/teamgbe/gameengine/Cell.java
@@ -1,4 +1,25 @@
 package ru.kpfu.itis.teamgbe.gameengine;
 
+import java.awt.*;
+
 public class Cell {
+    private Color color;
+    private String text;
+
+    public Cell() {
+    }
+
+    public Cell(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+
 }
