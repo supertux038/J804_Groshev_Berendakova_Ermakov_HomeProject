72381f5f3fbe2c0089028df23fac650264fa8701
Hyperbot <vitalij-groshev@mail.ru>
2018-12-13 22:19:55 +0300

    Font parameter added

--- src/ru/kpfu/itis/teamgbe/gameengine/BufferedImageGameDrawer.java
+++ src/ru/kpfu/itis/teamgbe/gameengine/BufferedImageGameDrawer.java
@@ -11,12 +11,63 @@ import java.awt.image.BufferedImage;
 public class BufferedImageGameDrawer {
     private BufferedImage bufferedImage;
     private Gamefield gamefield;
+    //probably we should create GamefieldDrawingParameters class for next properties
     private boolean gridEnabled = true;
     private int gridThickness = 1;
     private boolean coordinatesEnabled = true;
     private Color gridColor = Color.BLACK;
     private Color textColor = Color.BLACK;
+    private Font font;
 
+    public Font getFont() {
+        return font;
+    }
+
+    public void setFont(Font font) {
+        this.font = font;
+    }
+
+    public boolean isGridEnabled() {
+        return gridEnabled;
+    }
+
+    public void setGridEnabled(boolean gridEnabled) {
+        this.gridEnabled = gridEnabled;
+    }
+
+    public int getGridThickness() {
+        return gridThickness;
+    }
+
+    public void setGridThickness(int gridThickness) {
+        this.gridThickness = gridThickness;
+    }
+
+    public boolean isCoordinatesEnabled() {
+        return coordinatesEnabled;
+    }
+
+    public void setCoordinatesEnabled(boolean coordinatesEnabled) {
+        this.coordinatesEnabled = coordinatesEnabled;
+    }
+
+    public Color getGridColor() {
+        return gridColor;
+    }
+
+    public void setGridColor(Color gridColor) {
+        this.gridColor = gridColor;
+    }
+
+    public Color getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(Color textColor) {
+        this.textColor = textColor;
+    }
+
+    //maybe we don't need constructor and should give arguments straight to drawGamefield?
     public BufferedImageGameDrawer(int width, int height, Gamefield gamefield) {
         bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         this.gamefield = gamefield;
@@ -26,6 +77,10 @@ public class BufferedImageGameDrawer {
         Graphics bufferedImageGraphics = bufferedImage.createGraphics();
         int cellWidth = bufferedImage.getWidth()/ gamefield.getWIDTH();
         int cellHeight = bufferedImage.getHeight()/ gamefield.getHEIGHT();
+
+        if(font != null) {
+            bufferedImageGraphics.setFont(font);
+        }
 
         /* drawing cells and its fields */
         for(int i = 0; i < gamefield.getHEIGHT(); i++) {
