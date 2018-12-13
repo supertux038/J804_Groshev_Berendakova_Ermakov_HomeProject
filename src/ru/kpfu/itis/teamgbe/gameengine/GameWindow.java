97914b25319b631c6ceb051c93c0f3bf348906d9
Hyperbot <vitalij-groshev@mail.ru>
2018-12-13 22:20:35 +0300

    BufferedImageGameDrawer is now used in draw()

--- src/ru/kpfu/itis/teamgbe/gameengine/GameWindow.java
+++ src/ru/kpfu/itis/teamgbe/gameengine/GameWindow.java
@@ -15,12 +15,23 @@ import java.awt.image.BufferedImage;
 public class GameWindow extends JFrame {
     private JPanel panel;
     private Timer timer;
-    private final Game game;
+    private final Gamefield gamefield;
     private boolean gridEnabled = true;
     private int gridThickness = 1;
     private boolean coordinatesEnabled = true;
     private Color gridColor = Color.BLACK;
     private Color textColor = Color.BLACK;
+    private Font font;
+
+    @Override
+    public Font getFont() {
+        return font;
+    }
+
+    @Override
+    public void setFont(Font font) {
+        this.font = font;
+    }
 
     /**
      * the main constructor
@@ -28,11 +39,11 @@ public class GameWindow extends JFrame {
      * @param defaultWidth width in pixels with which window would appear (only appear, width can change during runtime)
      * @param defaultHeight height in pixels with which window would appear (only appear, height can change during runtime)
      * @param fps frames per second, how fast would the window be redrawn
-     * @param game the Game class instance, that would be drawn with this window
+     * @param gamefield the Gamefield class instance, that would be drawn with this window
      */
-    public GameWindow(String windowLabel, int defaultWidth, int defaultHeight, int fps, Game game) {
+    public GameWindow(String windowLabel, int defaultWidth, int defaultHeight, int fps, Gamefield gamefield) {
         super(windowLabel);
-        this.game = game;
+        this.gamefield = gamefield;
         this.setBounds(0,0, defaultWidth, defaultHeight);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -131,50 +142,18 @@ public class GameWindow extends JFrame {
     /** Function that draws new frame. It's called *fps* times per second (fps is set in constructor and can't be changed during the runtime)
      *
      */
-    public void draw() {
-        //creating imageBuffer to swap it at a moment to reduce fps flicker
-        BufferedImage bufferedImage = new BufferedImage(panel.getWidth(), panel.getHeight(), BufferedImage.TYPE_INT_RGB);
-        Graphics bufferedImageGraphics = bufferedImage.createGraphics();
-        int cellWidth = panel.getWidth()/game.getWIDTH();
-        int cellHeight = panel.getHeight()/game.getHEIGHT();
-
-        /* drawing cells and its fields */
-        for(int i = 0; i < game.getHEIGHT(); i++) {
-            for(int j = 0; j < game.getWIDTH(); j++) {
-                bufferedImageGraphics.setColor(game.gameField[i][j].getColor());
-                bufferedImageGraphics.fillRect(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
-                bufferedImageGraphics.setColor(textColor);
-                bufferedImageGraphics.drawString(game.gameField[i][j].getText(),i*cellWidth,j*cellHeight + cellHeight/2);
-            }
+    private void draw() {
+        BufferedImageGameDrawer bufferedImageGameDrawer = new BufferedImageGameDrawer(panel.getWidth(), panel.getHeight(), gamefield);
+        bufferedImageGameDrawer.setCoordinatesEnabled(coordinatesEnabled);
+        bufferedImageGameDrawer.setGridColor(gridColor);
+        bufferedImageGameDrawer.setGridEnabled(gridEnabled);
+        bufferedImageGameDrawer.setGridThickness(gridThickness);
+        bufferedImageGameDrawer.setTextColor(textColor);
+        if(font != null) {
+            bufferedImageGameDrawer.setFont(font);
         }
-
-        //drawing grid
-        if(gridEnabled) {
-            bufferedImageGraphics.setColor(gridColor);
-            for (int i = 1; i < game.getWIDTH(); i++) {
-                bufferedImageGraphics.fillRect(i*cellWidth,0,gridThickness,panel.getHeight());
-            }
-            for (int i = 1; i < game.getHEIGHT(); i++) {
-                bufferedImageGraphics.fillRect(0,i * cellHeight, panel.getWidth(), gridThickness);
-            }
-        }
-
-        //drawing coordinates
-        if(coordinatesEnabled) {
-            bufferedImageGraphics.setColor(textColor);
-            for (int i = 0; i < game.getHEIGHT(); i++) {
-                bufferedImageGraphics.drawString(Integer.toString(i + 1),1,i*cellHeight + cellHeight/2 );
-            }
-            for(int i = 0; i < game.getWIDTH(); i++) {
-                bufferedImageGraphics.drawString(Character.toString((char)('A' + i)),i*cellWidth + cellWidth /2, panel.getHeight() - 1 );
-            }
-        }
-
-        bufferedImageGraphics.dispose();
-
-        //swapping buffers
         Graphics panelGraphics = panel.getGraphics();
-        panelGraphics.drawImage(bufferedImage,0,0, null);
+        panelGraphics.drawImage(bufferedImageGameDrawer.drawGamefield() ,0,0, null);
         panelGraphics.dispose();
     }
 }
